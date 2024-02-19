17b09b4c425b88384099df14086742ee96d42cac
Mathews Kuriakose <mathews.kuriakose.007@gmail.com>
2024-02-19 22:34:56 +0530

    added config server

---
+++ config-server/src/test/java/com/mathewzvk/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathewzvk.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
