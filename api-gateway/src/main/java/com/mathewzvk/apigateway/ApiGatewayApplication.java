b0105ab9355f6ebcf561ac4fc6045f8f145a9131
Mathews Kuriakose <mathews.kuriakose.007@gmail.com>
2024-02-20 21:03:25 +0530

    added api gateway

---
+++ api-gateway/src/main/java/com/mathewzvk/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.mathewzvk.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+
+}
